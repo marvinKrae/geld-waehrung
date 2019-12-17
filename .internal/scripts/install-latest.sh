125c5aefa467df3e19fa941d9a9cfc4061384e58
Bjarne Koll <wi18270@lehre.dhbw-stuttgart.de>
2019-12-17 11:33:43 +0100

    Create clone section in install script
    This commit creates a new section in the install script that clones the
    project to the users local machine as well as setting up the git
    credential.helper store to remove the need from providing the dhbw
    gitlab credentials over and over again.
    
    This cloning also sets up the remotes in the created local repository,
    mainly upstream pointing to the latex-template repository and origin to
    the provided repository.

--- .internal/scripts/install-latest.sh
+++ .internal/scripts/install-latest.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Hard coded variables
+LATEX_TEMPLATE_HTTPS_GIT=https://git.dhbw-stuttgart.de/wi-lab/latex-template.git
+
+# Colors
 RESET=$(tput sgr0)
 BU=$(tput smul)
 AQUA=$(tput setaf 14)
@@ -6,8 +10,11 @@ DARK_AQUA=$(tput setaf 6)
 LIGHT_RED=$(tput setaf 9)
 GREEN=$(tput setaf 82)
 DARK_GREEN=$(tput setaf 2)
+GRAY=$(tput setaf 247)
 BOLD=$(tput bold)
+GOLD=$(tput setaf 11)
 
+# Utility methods
 function cecho() {
   output_string=""
   for var in "$@"; do
@@ -20,15 +27,16 @@ function cecho_yes_no() {
   cecho "$@"
   answer=""
   read answer
-  if [[ ${answer} =~ ^[Yy]$ ]]; then
+  if [[ ${answer} =~ ^[Yy]$ || -z ${answer} ]]; then
     return 0
   fi
   return 1
 }
+
 function new_topic() {
-    TOTAL_PRINTING_CHARACTERS=$(($(tput cols) * 2 / 5 ))
-    TOTAL_SEPARATORS=$((TOTAL_PRINTING_CHARACTERS - ${#2}))
-    SEPARATOR_AMOUNT=$((TOTAL_SEPARATORS / 2))
+    local TOTAL_PRINTING_CHARACTERS=$(($(tput cols) * 2 / 5 ))
+    local TOTAL_SEPARATORS=$((TOTAL_PRINTING_CHARACTERS - ${#2}))
+    local SEPARATOR_AMOUNT=$((TOTAL_SEPARATORS / 2))
 
     printf ${BOLD}
     for (( i = 0; i < ${SEPARATOR_AMOUNT}; ++i )); do
@@ -46,25 +54,54 @@ function new_topic() {
 }
 
 new_topic "${AQUA}" "[DHBW LaTeX Template Generator]"
+
 # Checking for the required tools on the machine, mostly git
 if ! hash git 2>/dev/null; then
   cecho "Could not find ${LIGHT_RED}git" " on your machine\n"
   exit 1
 fi
 
+# Request remote git repository in which the project will live
 REMOTE_GIT_REPOSITORY=$1
 if [[ -z ${REMOTE_GIT_REPOSITORY} ]]; then
+  cecho "${GRAY}Keep the following empty, if you do not plan on hosting the project on a remote.\n"
   cecho "Please provide the projects remote ${AQUA}${BU}git repository url" ": "
   read -r REMOTE_GIT_REPOSITORY
 fi
-cecho "The project will be hosted at: " "${AQUA}${BU}${REMOTE_GIT_REPOSITORY}\n"
+if [[ -z ${REMOTE_GIT_REPOSITORY} ]]; then
+    cecho "The project will ${LIGHT_RED}not" " be hosted at any repository.\n"
+else
+    cecho "The project will be hosted at: " "${AQUA}${BU}${REMOTE_GIT_REPOSITORY}.\n"
+fi
 
 new_topic "${GREEN}" "[Project Name]"
-PROJECT_NAME=$(echo "$REMOTE_GIT_REPOSITORY" | rev | cut -d '/' -f 1 | cut -c 5- | rev)
-while ! (cecho_yes_no "Do you want to use ${GREEN}${BU}${PROJECT_NAME}" " as your project name" " ${DARK_GREEN}[y/n]" "?"); do
+PROJECT_NAME=$(echo "${REMOTE_GIT_REPOSITORY:=latex-template.git}" | rev | cut -d '/' -f 1 | cut -c 5- | rev)
+while ! (cecho_yes_no "Do you want to use ${GREEN}${BU}${PROJECT_NAME}" " as your project name" " ${DARK_GREEN}[y/n]" "? "); do
   cecho "Please provide the ${GREEN}${BU}project name" ": ${DARK_AQUA}"
   read -r PROJECT_NAME
 done
 
-echo "Cloning dhbw latex template" "to ${GREEN}${BU}${PROJECT_NAME}"
+new_topic "${GOLD}" "Cloning up the project"
+cecho "${GOLD}Cloning" " the latest latex template to ${GOLD}${BU}${PROJECT_NAME}.\n"
+if ! git config credential.helper>/dev/null; then
+    cecho " ┏ You do not have the git ${GOLD}credential helper"  " enabled!\n"
+    cecho " ┃ The ${GOLD}credential helper" " allows you to ${GOLD}store" " your ${GOLD}git remote credentials" ".\n"
+    if cecho_yes_no " ┃ Do you want to enabled it" " ${DARK_GREEN}[y/n]" "? "; then
+        git config --global credential.helper store
+        cecho " ┗ ${DARK_GREEN}Enabled" " the git ${GOLD}credential helper" " globally!\n"
+    else
+        cecho " ┗ Did not enable git credential helper.\n"
+    fi
+fi
+git clone ${LATEX_TEMPLATE_HTTPS_GIT} ${PROJECT_NAME}
+
+cd ${PROJECT_NAME}
+git remote add upstream ${LATEX_TEMPLATE_HTTPS_GIT}
+if [[ -z ${REMOTE_GIT_REPOSITORY} ]]; then
+    git remote remove origin
+else
+    git remote set-url origin ${REMOTE_GIT_REPOSITORY}
+fi
+cecho "${GOLD}${BU}Done!\n"
+
 
