34671d678117d526dd613a5896a0cc7155f65182
Bjarne Koll <wi18270@lehre.dhbw-stuttgart.de>
2020-02-18 14:47:32 +0100

    Create gitlab ci module
    This commit creates a new module that is capable of installing a gitlab
    CI configuration file, more specifically the .gitlab-ci.yml.
    
    This CI configuration will compile the latex template on push into a
    final output pdf file and publish this as a job artifact.

--- .internal/modules/github-workflow/init.sh
+++ .internal/modules/github-workflow/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export MODULE_NAME="GitHub CI"
+export MODULE_NAME="GitHub Workflow"
 export MODULE_DESCRIPTION="Installs a simple github actions workflow for projects hosted on github.
 This workflow will automatically compile the latex project and provide the pdf as an artifact."
 
